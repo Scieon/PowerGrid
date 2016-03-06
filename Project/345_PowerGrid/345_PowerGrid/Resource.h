59d8a91ad49ac8da5541c431c4621fa88c8e0938
Geralastel <gian.m.colucci@gmail.com>
2016-03-05 19:56:10 -0500

    Update Resource.h

--- Project/345_PowerGrid/345_PowerGrid/Resource.h
+++ Project/345_PowerGrid/345_PowerGrid/Resource.h
@@ -1,29 +1,64 @@
-#ifndef RESOURCE_H
-#define RESOURCE_H
-
-#include <iostream>
-
+#pragma once
+#include <string>
 using namespace std;
 
-class Resource{
-public:
-	Resource();
-	~Resource();
-	Resource(int, int, int, int);
-	void setUranium(int uranium);
-	void setOil(int oil);
-	void setGarbage(int garbage);
-	void setCoal(int coal);
-	int getUranium();
-	int getOil();
-	int getGarbage();
-	int getCoal();
-
-private:
-	int uranium;
-	int oil;
-	int garbage;
-	int coal;
+class Resource {
+    
+    public :
+    
+    // getters and setters *set to virtual for better performance on inheritence*
+    virtual string get_color();
+    virtual string get_type();
+    virtual string get_shape();
+    virtual int get_cost();
+    virtual int get_quantity();
+    
+    virtual void set_type(string input);
+    virtual void set_shape(string input);
+    virtual void set_color(string input);
+    virtual void set_cost(int k);
+    virtual void set_quantity(int k);
+    virtual void resource_info();
+    Resource();
+    
+    protected :
+    
+    string type;
+    string shape;
+    string color;
+    int cost;
+    int quantity;
 };
 
-#endif //RESOURCE_H
+class Coal : public Resource{
+    
+public:
+    
+    Coal(int cost,int quantity);
+    
+};
+
+class Oil : public Resource{
+    
+public:
+    
+    Oil(int cost,int quantity);
+    
+};
+
+class Garbage : public Resource{
+    
+public:
+    
+    Garbage(int cost,int quantity);
+    
+    
+};
+
+class Uranium : public Resource{
+    
+public:
+    
+    Uranium(int cost,int quantity);
+    
+};
