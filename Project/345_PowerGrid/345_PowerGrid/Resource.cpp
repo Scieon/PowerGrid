ad1dde061c7fa77e85befd9e3e625c6f0b4d3746
Geralastel <gian.m.colucci@gmail.com>
2016-03-05 19:55:47 -0500

    Update Resource.cpp

--- Project/345_PowerGrid/345_PowerGrid/Resource.cpp
+++ Project/345_PowerGrid/345_PowerGrid/Resource.cpp
@@ -1,68 +1,105 @@
-#include <iostream>
+#include "Resource.h"
 #include <string>
-#include "Resource.h";
-
+#include <iostream>
 using namespace std;
 
-//Default constructor
+// Default Constructor
 Resource::Resource(){
-	uranium = 0;
-	oil = 0;
-	garbage = 0;
-	coal = 0;
-}
-//Constructor initializes resources 
-Resource::Resource(int uranium, int oil, int garbage, int coal){
-	this->uranium = uranium;
-	this->oil = oil;
-	this->garbage = garbage;
-	this->coal = coal;
+    color = "NA";
+    type = "NA";
+    shape = "NA";
+    cost = 0;
+    quantity = 0;
 }
 
-//Destructor
-Resource::~Resource(){
 
+// getters for resources
+
+string Resource::get_color(){
+    return color;
 }
 
-//Mutator function for setting the amount of uranium bought by a player
-void Resource::setUranium(int uranium){
-	this->uranium = uranium;
+string Resource::get_shape(){
+    return shape;
 }
 
-//Mutator function for setting the amount of oil bought by a player
-void Resource::setOil(int oil){
-	this->oil = oil;
+string Resource::get_type(){
+    return type;
 }
 
-//Mutator function for setting the amount of garbage bought by a player
-void Resource::setGarbage(int garbage){
-	this->garbage = garbage;
+int Resource::get_cost(){
+    return cost;
 }
 
-//Mutator function for setting the amount of coal bought by a player
-void Resource::setCoal(int coal){
-	this->coal = coal;
+int Resource::get_quantity(){
+    return quantity;
 }
 
-//Accessor function to get the amount of uranium bought by a player
-int Resource::getUranium(){
-	return uranium;
+// setters for class Resource
+void Resource::set_color(string input){
+    color = input;
 }
 
-//Accessor function to get the amount of garbage bought by a player
-int Resource::getGarbage(){
-	return garbage;
+void Resource::set_shape(string input){
+    shape = input;
 }
 
-//Accessor function to get the amount of oil bought by a player
-int Resource::getOil(){
-	return oil;
+void Resource::set_type(string input){
+    type = input;
 }
 
-//Accessor function to get the amount of coal bought by a player
-int Resource::getCoal(){
-	return coal;
+void Resource::set_cost(int k){
+    cost = k;
 }
 
+void Resource::set_quantity(int k){
+    quantity = k;
+}
+
+//Resource information method that returns type,cost,color,shape
+void Resource::resource_info(){
+    cout << endl << "Resource Characteristics" << endl
+    << "---------------------------" << endl;
+    cout << "Type: " << type << endl;
+    cout << "Color: " << color << endl;
+    cout << "Shape: " << shape << endl;
+    cout << "Cost: " << cost << endl;
+    cout << "---------------------------" << endl;
+}
+
+// Coal constructor
+Coal::Coal(int cost,int quantity){
+    color = "Brown";
+    type = "Coal";
+    shape = "Cube";
+    this->cost = cost;
+    this->quantity = quantity;
+}
+
+// Oil constructor
+Oil::Oil(int cost,int quantity){
+    color = "Black";
+    type = "Oil";
+    shape = "Cylinder";
+    this->cost = cost;
+    this->quantity = quantity;
+}
+
+// Garbage constructor
+Garbage::Garbage(int cost,int quantity){
+    color = "Yellow";
+    type = "Garbage";
+    shape = "Large Octagonal";
+    this->cost = cost;
+    this->quantity = quantity;
+}
+
+Uranium::Uranium(int cost,int quantity){
+    color = "Red";
+    type = "Uranium";
+    shape = "Small Octagonal";
+    this->cost = cost;
+    this->quantity = quantity;
+}
 
 
