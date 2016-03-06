22a15df33373c432d42b196ba81c9cfdda2cfef5
Geralastel <gian.m.colucci@gmail.com>
2016-03-05 20:12:09 -0500

    Update Player.h

--- Project/345_PowerGrid/345_PowerGrid/Player.h
+++ Project/345_PowerGrid/345_PowerGrid/Player.h
@@ -17,7 +17,7 @@ public:
 	int getTurnOrder();
 	string getColor();
 	int getMoney();
-    Resource *resource[4]; //Should put this private since its attribute
+   
 	HouseManager* getHouseManager();
 
 	//Power Plant methods added by Anhkhoi, feel free to modify
@@ -32,6 +32,7 @@ private:
 	int turnOrder;
 	string color;
 	HouseManager* houseManager;
+	Resource *resource[4]; 
 	Powerplant * powerplants[3]; //Players can have up to 3 power plants stored in an array of powerplants
 	int numberOfPlants = 0;  //Counter for each player
 	
