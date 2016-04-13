dc79ab7852d108d8b0f9d7841c315bc0a8847c09
Someone <test@test.com>
2016-04-13 09:52:05 -0400

    Change Powerplant Step 3 name

--- Project/345_PowerGrid/345_PowerGrid/Powerplantmanager.cpp
+++ Project/345_PowerGrid/345_PowerGrid/Powerplantmanager.cpp
@@ -58,7 +58,7 @@ PowerplantManager::PowerplantManager() {
 	Powerplant eco44(44, "Eco", 0, 5);
 	Powerplant eco50(50, "Eco", 0, 6);
 
-	Powerplant step3(-1, "Step 3", -1, -1);
+	Powerplant step3(-1, "Step_3", -1, -1);
 
 
 	powerplantsVector = new vector<Powerplant>();
